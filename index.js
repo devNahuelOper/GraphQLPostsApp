2138a6e9e638ecaf604d5c7c5ca2cfa0a9a28fdd
“nahuel” <n.gorosito@yahoo.com>
2021-03-14 19:10:42 -0400

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => res.send("Goodbye Cruel World"));
+const app = express();
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello Cruel World"));
+
+app.use(bodyParser.json());
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
