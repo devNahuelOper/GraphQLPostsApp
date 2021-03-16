a7087519b5779a69744197c028138be3d186252e
“nahuel” <n.gorosito@yahoo.com>
2021-03-15 20:17:54 -0400

    Build newPost mutation

--- schema/mutations.js
+++ schema/mutations.js
@@ -1,9 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLNonNull } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull } = graphql;
 const mongoose = require("mongoose");
 const UserType = require("./user_type");
+const PostType = require("./post_type");
 
 const User = mongoose.model("user");
+const Post = mongoose.model("post");
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -15,9 +17,27 @@ const mutation = new GraphQLObjectType({
         email: { type: new GraphQLNonNull(GraphQLString) },
         password: { type: new GraphQLNonNull(GraphQLString) },
       },
-      resolve(parentValue, { name, email, password}) {
-        return new User({ name, email, password}).save();
-      }
+      resolve(parentValue, { name, email, password }) {
+        return new User({ name, email, password }).save();
+      },
+    },
+    newPost: {
+      type: PostType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        body: { type: new GraphQLNonNull(GraphQLString) },
+        author: {
+          type: new GraphQLNonNull(GraphQLString),
+          resolve(parentValue) {
+            return User.findById(parentValue.author)
+              .then((user) => user)
+              .catch((err) => null);
+          },
+        },
+      },
+      resolve(parentValue, { title, body, author }) {
+        return new Post({ title, body, author }).save();
+      },
     },
   },
 });
